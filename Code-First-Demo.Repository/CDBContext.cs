f0d6f5a6a08442c7c47432f464b828057bab7ae5
Dracokm <dracokm@gmail.com>
2018-11-26 12:12:02 +0000

    Messing around with CRUD stuff

---
+++ Code-First-Demo.Repository/CDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Code_First_Demo.Models;
+
+namespace Code_First_Demo.Repository
+{
+    public class CDBContext : DbContext
+    {
+        public CDBContext() : base("StringDBContext")
+        {
+        }
+        public DbSet<MyFirstTable> MyFirstTable { get; set; }
+    }
+}
